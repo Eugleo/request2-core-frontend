dede8161ec5bfe4d03c94e4434594ac3a9441884
Evžen Wybitul <wybitul.evzen@gmail.com>
2020-03-16 00:10:06 +0100

    Remove old frontend code

--- src/Api.js
+++
@@ -1,7 +0,0 @@
-
-export const api_endpoint='http://localhost:9080/';
-
-export function api_get(what, fetchArgs={}) {
-	fetchArgs.method = "GET";
-	return fetch(api_endpoint+what, fetchArgs).then((r)=>r.json());
-}
