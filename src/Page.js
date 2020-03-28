2adcb0ea5cf522104902e34f57b2e4b2b790b36e
Evžen Wybitul <wybitul.evzen@gmail.com>
2020-03-28 18:01:29 +0100

    Refactor forms to use Formik

--- src/Page.js
+++ src/Page.js
@@ -5,7 +5,28 @@ export default function Page(props) {
 
   return (
     <div className={classes.join(" ")}>
-      <h1 className="text-3xl font-bold leading-tight mb-4 text-black mt-8">{props.title}</h1>
+      <h1 className="text-3xl font-bold leading-tight text-black mt-8">{props.title}</h1>
+      <div className="mt-6">{props.children}</div>
+    </div>
+  );
+}
+
+export function CenteredPage(props) {
+  let classes = [
+    "w-full",
+    "h-full",
+    props.width || "",
+    "px-6",
+    "flex-grow",
+    "mx-auto",
+    "flex",
+    "flex-col",
+    "justify-center"
+  ];
+
+  return (
+    <div className={classes.join(" ")}>
+      <h1 className="text-3xl text-center font-bold leading-tight text-black">{props.title}</h1>
       <div className="mt-6">{props.children}</div>
     </div>
   );
