28b537769d4c858ec5b3610b44f8b189ca01d941
Evžen Wybitul <wybitul.evzen@gmail.com>
2020-08-09 14:47:18 +0200

    Add isJust

--- src/Utils/Maybe.ts
+++ src/Utils/Maybe.ts
@@ -3,3 +3,7 @@ export type Maybe<T> = T | null | undefined;
 export function maybe<T, U>(x: Maybe<T>, f: (x: T) => U, def?: U): U | undefined {
   return x ? f(x) : def;
 }
+
+export function isJust<T>(x: Maybe<T>): x is T {
+  return x !== null && x !== undefined;
+}
