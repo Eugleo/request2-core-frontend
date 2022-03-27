b6c21c219f328442aa747f1c10defcd4a520c7f9
eugleo <wybitul.evzen@gmail.com>
2022-03-27 20:07:49 +0200

    Add some form of inline image rendering

--- src/Request/FileView.tsx
+++ src/Request/FileView.tsx
@@ -4,19 +4,52 @@ import { File } from 'react-feather';
 
 import { apiBase } from '../Utils/ApiBase';
 import { FileInfo } from '../Utils/File';
+import { comparing } from '../Utils/Func';
+
+const PLACEHOLDERS = [
+  'https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif',
+  'https://c.tenor.com/27-GDSe8tRkAAAAM/zolw-sobie-tanczy.gif',
+  'https://www.tom-archer.com/wp-content/uploads/2018/06/milford-sound-night-fine-art-photography-new-zealand.jpg',
+];
 
 export function FilesView({ files }: { files: FileInfo[] }): JSX.Element {
   if (files.length > 0) {
     return (
-      <div>
-        {files.map(f => (
-          <a key={f.hash} href={`${apiBase}/files/${f.hash}`} className="text-gray-800">
-            <div className="flex flex-row items-center py-1 px-2 rounded-sm hover:bg-gray-50">
-              <File className="h-3 w-3 text-gray-700 mr-2" />
-              <p className="text-gray-800 text-sm">{f.name}</p>
-            </div>
-          </a>
-        ))}
+      <div className="pt-2 grid grid-cols-3 gap-4">
+        {files
+          .map(f => ({ ...f, isImage: /image\/*/u.test(f.mime) }))
+          .sort(comparing(f => f.isImage))
+          .map(f => {
+            return (
+              <div
+                key={f.hash}
+                className={c(
+                  'text-gray-800 rounded-lg border border-gray-300 shadow-sm hover:bg-gray-50',
+                  f.isImage && 'col-span-3'
+                )}
+              >
+                <a href={`${apiBase}/files/${f.hash}`} className="px-4 py-3 block">
+                  <div className="flex flex-row items-center gap-3">
+                    {f.isImage ? null : (
+                      <div className="rounded-full w-10 h-10 flex flex-row items-center justify-center bg-indigo-100">
+                        <File className="h-5 w-5 text-indigo-800 stroke-3" />
+                      </div>
+                    )}
+                    {f.isImage ? (
+                      <div className="flex flex-row justify-center bg-gray-50 mb-2">
+                        <img
+                          className="block"
+                          alt="placeholder"
+                          src={PLACEHOLDERS[Math.floor(Math.random() * PLACEHOLDERS.length)]}
+                        />
+                      </div>
+                    ) : null}
+                    <p className="text-gray-800 text-sm">{f.name}</p>
+                  </div>
+                </a>
+              </div>
+            );
+          })}
       </div>
     );
   }
